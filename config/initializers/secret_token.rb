2771206a1fd2c1f1d59d138bad6aaccc113a4375
Jaime Moreno <j7isaac@gmail.com>
2016-04-08 04:09:47 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Empires::Application.config.secret_key_base = '026facc8a7dd9a3a0353244b1279ff6ba2d71def92b826d16753cb2377bf6eebc5dbdf44eef647b7ddb96881730ffc5ee5ec29d100ac4add3c7dd953f58b54b0'
