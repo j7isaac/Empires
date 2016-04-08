dbc3e44baa3a563012711db736dc94e797654daa
j7isaac <j7issac@gmail.com>
2016-04-08 09:40:24 -0500

    Created and defined Battles Controller

---
+++ app/controllers/battles_controller.rb
@@ -0,0 +1,26 @@
+class BattlesController < ApplicationController
+
+  helper_method :battle
+
+  def new
+    @battle = Battle.new
+  end
+
+  def create
+    @battle = Battle.create(battle_params)
+    redirect_to @battle
+  end
+
+  def show
+    @battle = Battle.find(params[:id])
+  end
+
+  private
+
+  def battle
+    @battle ||= Battle.where(id: params[:id]).last
+  end
+
+  def battle_params
+    params.require(:battle).permit(:army_a, :army_b)
+end
