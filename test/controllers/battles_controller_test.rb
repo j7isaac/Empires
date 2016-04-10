735dfd833bd42e7780ca1284c7e054dcbb155d36
j7isaac <j7issac@gmail.com>
2016-04-10 16:37:20 -0500

    Added Battles Controller Tests
    - Added “Should Get New” test
    - Added “Should Create a New Battle Object” test
    - Added “Should Create a Battle That Persists and Redirects to show”
    test

--- test/controllers/battles_controller_test.rb
+++ test/controllers/battles_controller_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class BattlesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get new" do
+    get :new
+    assert_response :success
+    assert_template 'battles/new'
+  end
+
+  test "should create a new battle object" do
+    get :new
+    battle = assigns(:battle) 
+    assert battle.new_record?
+    assert_response :success
+  end
+
+  test "should create a battle that persists and redirects to show" do
+    params = {
+      battle: {army_a: "test_army", army_b: "test_army_2"}
+    }
+    post :create, params
+    battle = assigns(:battle)
+    assert battle.persisted?
+    assert_redirected_to battle
+  end
 end
