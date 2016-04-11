0c65cb9a6bf208da22ace1402500ce3c20ede44c
j7isaac <j7issac@gmail.com>
2016-04-11 13:39:32 -0500

    Correction to battle.rb

--- app/models/battle.rb
+++ app/models/battle.rb
@@ -5,18 +5,16 @@ class Battle < ActiveRecord::Base
   def initiate_battle!
     a_army = Army.find {|a| a.id == army_a }
     b_army = Army.find {|a| a.id == army_b }
-    
     while a_army.soldiers_remaining.present? && b_army.soldiers_remaining.present? do
       a_army.army_attack(b_army.soldiers_remaining)
       b_army.army_attack(a_army.soldiers_remaining)
     end
     victor(a_army.soldiers_remaining, b_army.soldiers_remaining)
-
   end
 
   def victor(a, b)
-    "#{a_army.name} won!" if a.count > b.count
-    "#{b_army.name} won!" if b.count > a.count
+    "#{army_a.name} is victorious!" if a.count > b.count
+    "#{army_b.name} is victorious!" if b.count > a.count
   end
 
 
