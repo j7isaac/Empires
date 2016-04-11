b1a7df4d5f8b7090a8a0c0e173ef0d2c0b4c2241
j7isaac <j7issac@gmail.com>
2016-04-11 00:19:04 -0500

    Added Random Army Method to Battles Model

--- app/models/battle.rb
+++ app/models/battle.rb
@@ -1,2 +1,12 @@
 class Battle < ActiveRecord::Base
+  has_many :armies
+  after_create :initiate_battle!
+
+  def initiate_battle!
+
+  end
+
+  def random_army
+    [army_a, army_b].sample
+  end
 end
