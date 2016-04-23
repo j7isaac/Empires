c434f200d7f78bdbdff06e614d95e855f698b590
j7isaac <j7issac@gmail.com>
2016-04-22 19:25:15 -0500

    Altered the params in armies_controller_test.rb

--- test/controllers/armies_controller_test.rb
+++ test/controllers/armies_controller_test.rb
@@ -16,7 +16,12 @@ class ArmiesControllerTest < ActionController::TestCase
 
   test 'should create a battle that persists and redirects to new' do
     params = {
-      army: { name: 'test_army', infantry: 30, archers: 60, knights: 120 }
+      army: {
+        name: 'test_army',
+        infantry: 30,
+        archers: 60,
+        knights: 120
+      }
     }
     post :create, params
     army = assigns(:army)
