461e08e81a3fd5d79f0d36bc9cbb31a97acfbed1
j7isaac <j7issac@gmail.com>
2016-04-22 19:00:22 -0500

    Cleaned up armies_controller_test.rb

--- test/controllers/armies_controller_test.rb
+++ test/controllers/armies_controller_test.rb
@@ -1,22 +1,22 @@
 require 'test_helper'
 
 class ArmiesControllerTest < ActionController::TestCase
-  test "should get new" do
+  test 'should get new' do
     get :new
     assert_response :success
     assert_template 'armies/new'
   end
 
-  test "should create a new battle object" do
+  test 'should create a new battle object' do
     get :new
-    army = assigns(:army) 
+    army = assigns(:army)
     assert army.new_record?
     assert_response :success
   end
 
-  test "should create a battle that persists and redirects to new" do
+  test 'should create a battle that persists and redirects to new' do
     params = {
-      army: {name: "test_army", infantry: 30, archers: 60, knights: 120}
+      army: { name: 'test_army', infantry: 30, archers: 60, knights: 120 }
     }
     post :create, params
     army = assigns(:army)
