c26e3dd8f8ba506bdc80478213ee4c607d220af8
j7isaac <j7issac@gmail.com>
2016-04-22 19:22:45 -0500

    Cleaned up battles_controller_test.rb

--- test/controllers/battles_controller_test.rb
+++ test/controllers/battles_controller_test.rb
@@ -1,28 +1,29 @@
 require 'test_helper'
 
 class BattlesControllerTest < ActionController::TestCase
-
   def setup
-    @A1 = Army.create(id: 2, name: "army1", infantry: 20, archers: 20, knights: 20)
-    @A2 = Army.create(id: 3, name: "army2", infantry: 20, archers: 20, knights: 20)
-  end 
+    @a1 = armies(:one)
+    @a2 = armies(:two)
+    @a1.build_army!
+    @a2.build_army!
+  end
 
-  test "should get new" do
+  test 'should get new' do
     get :new
     assert_response :success
     assert_template 'battles/new'
   end
 
-  test "should create a new battle object" do
+  test 'should create a new battle object' do
     get :new
-    battle = assigns(:battle) 
+    battle = assigns(:battle)
     assert battle.new_record?
     assert_response :success
   end
 
-  test "should create a battle that persists and redirects to show" do
+  test 'should create a battle that persists and redirects to show' do
     params = {
-      battle: {army_a: @A1.id, army_b: @A2.id}
+      battle: { army_a: @a1.id, army_b: @a2.id }
     }
     post :create, params
     battle = assigns(:battle)
